b593416d363f1f17469cfb9ec2440c295a87ab90
Niels Möller <nisse@lysator.liu.se>
2024-10-30 10:06:04 +0100

    Include count in the copied state

--- hmac-internal.h
+++ hmac-internal.h
@@ -44,14 +44,10 @@ _nettle_hmac_set_key (size_t state_size, void *outer, void *inner,
 		      const struct nettle_hash *hash,
 		      size_t key_size, const uint8_t *key);
 
-#define _NETTLE_HMAC_DIGEST(outer, inner, ctx, digest_size, f, length, digest) do { \
-    f ((ctx), (digest_size), (ctx)->block);				\
-    memcpy (&(ctx)->state, (outer), sizeof ((ctx)->state));		\
-    (ctx)->count = 1;							\
-    (ctx)->index = (digest_size);						\
+#define _NETTLE_HMAC_DIGEST(outer, inner, ctx, f, length, digest) do { \
+    memcpy (&(ctx)->state, (outer), sizeof (outer));			\
     f ((ctx), (length), (digest));					\
-    memcpy (&(ctx)->state, (inner), sizeof ((ctx)->state));		\
-    (ctx)->count = 1;							\
+    memcpy (&(ctx)->state, (inner), sizeof ((inner)));			\
   } while (0)
 
 #endif /* NETTLE_HMAC_INTERNAL_H_INCLUDED */
